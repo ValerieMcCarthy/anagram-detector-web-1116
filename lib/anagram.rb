0a2776464e66477bf03aa55d00edb02a94e0477d
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-30 17:12:42 -0500

    Anagram built.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize (word)
+	@word = word
+end
+
+def match (word_array)
+	final_array = []
+	word_array.each do |item|
+		if @word == item.split("").sort.join
+			final_array << item
+		end
+
+	end
+	final_array
+end
+
+#completed anagram
+
+end
